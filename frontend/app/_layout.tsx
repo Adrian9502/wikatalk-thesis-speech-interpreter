838c41a2a730f560fecc0316dc0eff935e72a144
Adrian <bontojohnadrian@gmail.com>
2025-02-15 16:27:42 +0800

    welcome screen done

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -40,9 +40,13 @@ const RootLayout = () => {
   return (
     // <GlobalProvider>
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
     //  </GlobalProvider>
   );
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from "react-native";
-
+import { Text, View, Pressable, Image, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 export default function Index() {
   return (
-    <View className="bg-red-500 p-3">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-emerald-500 h-screen relative flex items-center justify-around flex-1">
+      <StatusBar style="dark" />
+      <View className="absolute -top-[50vh] w-[140vw] rounded-full h-[100vh] bg-white"></View>
+      <View className=" w-full flex items-center justify-center">
+        <Image
+          className="w-full"
+          source={require("../assets/images/WikaTalk-logo.png")}
+          resizeMode="contain"
+        />
+        <View className="w-full items-center justify-center">
+          <Text className="font-pbold z-50 text-[3rem] text-emerald-500 text-center">
+            WikaTalk
+          </Text>
+          <Text className="font-pregular text-xl text-emerald-500">
+            Lorem ipsum dolor amet
+          </Text>
+        </View>
+      </View>
+      <View className="py-5 px-16 w-full flex gap-4">
+        <TouchableOpacity
+          activeOpacity={0.9}
+          className="bg-white p-3 rounded-xl"
+          onPress={() => router.push("/SignIn")}
+        >
+          <Text className="text-emerald-700 font-pregular text-lg uppercase text-center">
+            sign in
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.9}
+          className="bg-white p-3 rounded-xl"
+          onPress={() => router.push("/SignUp")}
+        >
+          <Text className="text-emerald-700 font-pregular text-lg uppercase text-center">
+            sign up
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
