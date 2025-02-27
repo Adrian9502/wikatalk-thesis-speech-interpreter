4cbc2238c6e10feb52636073bf6ab254aff9d34b
rbcurzon_laptop <curzonronald@gmail.com>
2025-02-27 09:05:27 +0800

    feat: add speech-to-speech translation to the home

--- frontend/app/(tabs)/Home.tsx
+++ frontend/app/(tabs)/Home.tsx
@@ -1,10 +1,143 @@
 import { SafeAreaView, Text, View, Image } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import MicButton from "@/components/MicButton";
+import { Audio } from "expo-av";
+import axios, { AxiosResponse } from "axios";
+import FormData from "form-data";
+import * as Speech from "expo-speech";
+
 const Home = () => {
-  const [tagalogText, setTagalogText] = useState("");
-  const [bisayaText, setBisayaText] = useState("");
+  const [upperTextfield, setUpperTextfield] = useState("...");
+  const [bottomTextfield, setBottomTextfield] = useState("...");
+  const [language1, setLanguage1] = useState("Tagalog");
+  const [language2, setLanguage2] = useState("Cebuano");
+  const [recording, setRecording] = useState<Audio.Recording | undefined>(
+    undefined
+  );
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const [user, setUser] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  async function startRecording() {
+    try {
+      if (permissionResponse?.status !== "granted") {
+        console.log("Requesting permission..");
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  useEffect(() => {
+    if (loading) {
+      console.log("loading..");
+    }
+  }, [loading]);
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording?.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording?.getURI();
+    console.log("Recording stopped and stored at", uri);
+    if (!uri) {
+      console.error("No recording URI found");
+      return;
+    }
+  }
+
+  async function translateRecord(srcLang: string, tgtLang: string) {
+    const FILEUPLOAD_URL = process.env.EXPO_PUBLIC_NGROK_FILEUPLOAD_URL;
+    if (!FILEUPLOAD_URL) {
+      console.error("No file upload URL found");
+      return;
+    }
+    const uri = recording?.getURI();
+    const formData = new FormData();
+    const filetype = uri?.split(".").pop();
+    const filename = uri?.split("/").pop();
+    setLoading(true);
+    formData.append("file", {
+      uri: uri,
+      type: `audio/${filetype}`,
+      name: filename,
+    });
+    formData.append("tgtLang", tgtLang);
+    formData.append("srcLang", srcLang);
+    console.log(formData);
+    console.log("File upload URL: ", FILEUPLOAD_URL);
+    axios
+      .post(FILEUPLOAD_URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        setLoading(false);
+        console.log("Response: ", response.data);
+        handleTextfield(
+          response?.data["translated_text"],
+          response?.data["transcribed_text"]
+        );
+        synthesis(response?.data["translated_text"]);
+      })
+      .catch((error) => {
+        console.log("Error translationg record: ", error);
+      });
+  }
+
+  const synthesis = (text: string, language: string = "fil-PH") => {
+    Speech.speak(text, { language: language, rate: 0.75 });
+  };
+
+  const handleTextfield = (text1: string, text2: string) => {
+    if (user == 1) {
+      setUpperTextfield(text1);
+      setBottomTextfield(text2);
+    } else {
+      setUpperTextfield(text2);
+      setBottomTextfield(text1);
+    }
+  };
+
+  const handlePress = async (event: number) => {
+    if (event == 1) {
+      console.log("Mic 1 pressed!");
+      setUser(1);
+      if (recording) {
+        await stopRecording();
+        await translateRecord(language1, language2);
+      } else {
+        startRecording();
+      }
+    } else if (event == 2) {
+      console.log("Mic 2 pressed!");
+      setUser(2);
+      if (recording) {
+        await stopRecording();
+        await translateRecord(language2, language1);
+      } else {
+        startRecording();
+      }
+    }
+  };
 
   return (
     <SafeAreaView className="h-screen bg-white flex items-center justify-around flex-1 pt-10">
@@ -12,23 +145,21 @@ const Home = () => {
       <View className="flex items-center relative justify-center w-full h-full">
         {/* Top section */}
         <View className="w-full h-1/2 bg-white pt-6 flex items-center justify-end relative">
-          <View className="p-3 absolute top-12 border-4 border-emerald-500 rounded-full">
-            <Image
+          <View className="p-3 absolute top-12 border-4 border-emerald-500 rounded-full ">
+            <MicButton
               source={require("@/assets/images/mic-emerald.png")}
-              className="w-12 h-12 rotate-180"
+              imgClassname={"w-12 h-12 rotate-180"}
+              onPress={() => handlePress(2)}
             />
           </View>
           <Text className="text-emerald-500 font-pregular rotate-180 text-2xl p-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
-            placeat id asperiores excepturi sapiente quod nihil facilis tenetur
-            odio nobis, aut voluptate, totam reiciendis repellat ad ipsam
-            dolores iusto tempora!
+            {upperTextfield}
           </Text>
         </View>
         {/* Exchange icon and language translation */}
         <View className="flex-row relative items-center justify-between w-full">
           <Text className="font-pregular text-emerald-500 w-1/2 text-xl py-3 px-2">
-            Bisakol
+            {language2}
           </Text>
 
           {/* Centered Circular Icon */}
@@ -37,21 +168,19 @@ const Home = () => {
           </View>
 
           <Text className="font-pregular py-3 px-2 text-white w-1/2 bg-emerald-500 text-xl text-end rotate-180">
-            Tangalog
+            {language1}
           </Text>
         </View>
         {/* bottom section */}
         <View className="w-full relative h-1/2 bg-emerald-500 pb-6 flex items-center justify-start p-3">
           <Text className="text-white p-5 text-2xl font-pregular">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            repellat dolor nisi ad libero, dignissimos error amet est ut
-            obcaecati doloribus asperiores sit, facere reiciendis nemo
-            architecto. Labore, iste officiis?
+            {bottomTextfield}
           </Text>
           <View className="absolute bottom-12 p-3 border-4 border-white bg-emerald-500 rounded-full">
-            <Image
+            <MicButton
               source={require("@/assets/images/mic-white.png")}
-              className="w-12 h-12"
+              onPress={() => handlePress(1)}
+              imgClassname="w-12 h-12"
             />
           </View>
         </View>
