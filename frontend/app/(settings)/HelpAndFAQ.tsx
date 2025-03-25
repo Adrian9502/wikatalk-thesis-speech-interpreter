ec3204da3eec3f50ffb53e43f474242b9dbc0d4a
Adrian <bontojohnadrian@gmail.com>
2025-03-25 11:43:34 +0800

    feat: implement theme background and dynamic styles for containers

--- frontend/app/(settings)/HelpAndFAQ.tsx
+++ frontend/app/(settings)/HelpAndFAQ.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import useThemeStore from "@/store/useThemeStore";
+import { getGlobalStyles } from "@/styles/globalStyles";
 const HelpAndFAQ = () => {
+  // Theme store
+  const { activeTheme } = useThemeStore();
+
+  // Get the dynamic styles based on the current theme
+  const dynamicStyles = getGlobalStyles(activeTheme.backgroundColor);
   return (
-    <View>
+    <SafeAreaView style={dynamicStyles.container}>
       <Text>HelpAndFAQ</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
